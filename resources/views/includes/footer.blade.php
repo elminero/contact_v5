287f1ab30475c698a8401c81f82e274273719518
Robert Farber <elminero@cox.net>
2017-08-23 01:41:52 +0000

    Made footer dynamic

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,4 +1,18 @@
 <section class="col-sm-8">
-    Current IP Address: 192.168.10.1    </section>
+    {{"Current IP Address: " . $_SERVER['REMOTE_ADDR']}}
+</section>
 <section class="col-sm-4 ">
-    Saturday 19th of August 2017    </section>
+    {{ Carbon\carbon::now(file_get_contents('https://ipapi.co/1.2.3.4/timezone/'))->format('l jS \\of F Y') }}
+</section>
+
+
+<!--
+Eastern ........... America/New_York
+Central ........... America/Chicago
+Mountain .......... America/Denver
+Mountain no DST ... America/Phoenix
+Pacific ........... America/Los_Angeles
+Alaska ............ America/Anchorage
+Hawaii ............ America/Adak
+Hawaii no DST ..... Pacific/Honolulu
+-->
